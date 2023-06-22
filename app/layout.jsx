6a9a22fcea133f54772e6a03ef0abf393db63463
pranatiY2000 <pranatiyanamandra2000@gmail.com>
2023-06-22 13:36:18 +0530

    Fixing Typo

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import Provider from '@components/Provider'
-import Nav from '@components/nav'
+import Nav from '@components/Nav'
 import '@styles/globals.css'
 export const metadata = {
     title: "Vendor Application",
